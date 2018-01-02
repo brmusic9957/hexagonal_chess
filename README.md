ceb91e9520792fe1ecb1e853dea86013b6d4f04d
guyo14 <alejandro1213@gmail.com>
2018-01-01 21:49:39 -0600

    sets up the project

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
 # hexagonal_chess
+
+Simple chess game using an hexagonal board.
+
+## What am I using?
+
++ JavaFX
++ Java2D libraries
++ Gradle
+
+## How to run it?
+
+### Unix based systems
+
+```bash
+.gradlew run
+```
+
+### Windows
+
+```bash
+gradlew.bat run
+```
---
+++ src/main/java/com/provech/chess/App.java
@@ -0,0 +1,21 @@
+package com.provech.chess;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+public class App extends Application{
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        primaryStage.setTitle("Hello World!");
+        StackPane root = new StackPane();
+        primaryStage.setScene(new Scene(root, 500, 500));
+        primaryStage.show();
+    }
+}
